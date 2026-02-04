88de9f345da6b65ad1be26183b31c1fce6838c78
mehmet388 <9026462@student.zadkine.nl>
2026-02-04 21:13:20 +0100

    Add files via upload

---
+++ Les 1 barry python/les1_student.py
@@ -0,0 +1,14 @@
+# Een class is een blauwdruk waarmee objecten worden gemaakt.
+# Een object is een concrete instantie van die class.
+
+class Student:
+    pass  # tijdelijke placeholder
+
+
+# Objecten aanmaken
+student1 = Student()
+student2 = Student()
+
+# Objecten printen
+print(student1)
+print(student2)
